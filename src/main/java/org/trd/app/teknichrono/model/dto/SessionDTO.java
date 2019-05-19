d037fb86b5ed9200ea78a5ecc7cb97685c33c17d
Loic Rouchon <loic.rouchon@gmail.com>
2019-05-19 18:42:41 +0200

    Introduce lombok for easier DTO generation

--- src/main/java/org/trd/app/teknichrono/model/dto/SessionDTO.java
+++ src/main/java/org/trd/app/teknichrono/model/dto/SessionDTO.java
@@ -1,6 +1,7 @@
 package org.trd.app.teknichrono.model.dto;
 
 import fr.xebia.extras.selma.Selma;
+import lombok.Data;
 import org.trd.app.teknichrono.model.dto.mapper.SessionMapper;
 import org.trd.app.teknichrono.model.jpa.Session;
 
@@ -10,12 +11,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
 public class SessionDTO {
 
     private final static SessionMapper MAPPER = Selma.builder(SessionMapper.class).build();
 
-    private long id;
-    private int version;
+    private Long id;
     private Instant start;
     private long inactivity = 60000L;
     private Instant end;
@@ -26,110 +27,6 @@ public class SessionDTO {
     private NestedLocationDTO location;
     private NestedEventDTO event = null;
     private Set<NestedPilotDTO> pilots = new HashSet<>();
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public int getVersion() {
-        return version;
-    }
-
-    public void setVersion(int version) {
-        this.version = version;
-    }
-
-    public Instant getStart() {
-        return start;
-    }
-
-    public void setStart(Instant start) {
-        this.start = start;
-    }
-
-    public long getInactivity() {
-        return inactivity;
-    }
-
-    public void setInactivity(long inactivity) {
-        this.inactivity = inactivity;
-    }
-
-    public Instant getEnd() {
-        return end;
-    }
-
-    public void setEnd(Instant end) {
-        this.end = end;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public boolean isCurrent() {
-        return current;
-    }
-
-    public void setCurrent(boolean current) {
-        this.current = current;
-    }
-
-    public List<NestedChronometerDTO> getChronometers() {
-        return chronometers;
-    }
-
-    public void setChronometers(List<NestedChronometerDTO> chronometers) {
-        this.chronometers = chronometers;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public NestedLocationDTO getLocation() {
-        return location;
-    }
-
-    public void setLocation(NestedLocationDTO location) {
-        this.location = location;
-    }
-
-    public NestedEventDTO getEvent() {
-        return event;
-    }
-
-    public void setEvent(NestedEventDTO event) {
-        this.event = event;
-    }
-
-    public Set<NestedPilotDTO> getPilots() {
-        return pilots;
-    }
-
-    public void setPilots(Set<NestedPilotDTO> pilots) {
-        this.pilots = pilots;
-    }
-
-    @Override
-    public String toString() {
-        return "SessionDTO{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
-    }
 
     public static SessionDTO fromSession(Session session) {
         return MAPPER.asSessionDto(session);
