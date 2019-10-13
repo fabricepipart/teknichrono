2500a363d1c15911b18a50b062ad449cd32d8453
Fabrice Pipart <fabrice.pipart@gmail.com>
2019-10-13 08:11:27 +0200

    Checkup client coverage

--- src/main/java/org/trd/app/teknichrono/rest/RestApplication.java
+++ src/main/java/org/trd/app/teknichrono/rest/RestApplication.java
@@ -1,35 +1,15 @@
 package org.trd.app.teknichrono.rest;
 
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import javax.transaction.Transactional;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
-import java.util.HashMap;
-import java.util.Map;
 
 @ApplicationPath("/rest")
 @Transactional
 public class RestApplication extends Application {
 
   public RestApplication() {
-    overrideToLocalDB();
+    super();
   }
 
-  private void overrideToLocalDB() {
-    Map<String, String> env = System.getenv();
-    Map<String, Object> configOverrides = new HashMap<String, Object>();
-    boolean remoteDB = false;
-    for (String envName : env.keySet()) {
-      if (envName.contains("DB_USERNAME")) {
-        remoteDB = true;
-      }
-      // You can put more code in here to populate configOverrides...
-    }
-    if (remoteDB) {
-      configOverrides.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
-      EntityManagerFactory emf = Persistence.createEntityManagerFactory("teknichrono-persistence-unit",
-          configOverrides);
-    }
-  }
 }
